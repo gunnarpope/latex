25fea0d899207234073331612cc6a4e4590933c8
gunnar pope <gunnar@gmail.com>
2019-03-02 16:13:00 -0500

    updating texify.sh

--- texify.sh
+++ texify.sh
@@ -1,14 +1,17 @@
-!#/bin/bash
+!#/bash/bin
 
 # by: gunnar pope
 # date: 2/6/19
 # automate the building and opening of a .tex file
 
 if [[ $# -ne 1 ]]; then
-	echo "USAGE: ./build.sh filename.tex"
-	echo "The output PDF is written to tmp/ directory"
+    echo "USAGE: ./build.sh filename.tex"
+    echo "The output PDF is written to pdf/ directory"
         exit 1
 fi 
 
-mkdir -p tmp
-pdflatex -output-directory=tmp -halt-on-error $1 && xdg-open tmp/${1::-4}.pdf 
+mkdir -p pdf 
+pdflatex -output-directory=pdf -aux-directory=pdf -halt-on-error $1 &&
+bibtex pdf/${1::-4}.aux &&
+pdflatex -output-directory=pdf -aux-directory=pdf -halt-on-error $1 &&
+xdg-open pdf/${1::-4}.pdf
